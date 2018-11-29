9166e918a547fc86ed0a6a44df71bc9f293ef989
madadr <madadr@outlook.com>
2018-11-29 12:07:32 +0100

    Implemented API gateway

--- scripts/dotnet-publish.ps1
+++ scripts/dotnet-publish.ps1
@@ -1,5 +1,5 @@
 cd ../src
-#dotnet publish --force ./Efforteo.Api -c Debug -o ./bin/Docker
+dotnet publish --force ./Efforteo.Services.Api -c Debug -o ./bin/Docker
 dotnet publish --force ./Efforteo.Services.Activities -c Debug -o ./bin/Docker
 dotnet publish --force ./Efforteo.Services.Authentication -c Debug -o ./bin/Docker
 dotnet publish --force ./Efforteo.Services.Accounts -c Debug -o ./bin/Docker
---
+++ src/Efforteo.Services.Api/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Efforteo.Services.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => CreateWebHostBuilder(args).Build().Run();
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args)
+            => WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((host, config) => { config.AddJsonFile("ocelot.json"); })
+                .UseStartup<Startup>();
+    }
+}
