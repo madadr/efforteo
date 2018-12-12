839a5c7d9757c5807b095abef66e97690d7f2817
madadr <madadr@outlook.com>
2018-12-12 14:34:49 +0100

    Added validation in add-activity view

--- src/Efforteo.WebApp/efforteo-webapp/src/app/add-activity/add-activity.component.ts
+++ src/Efforteo.WebApp/efforteo-webapp/src/app/add-activity/add-activity.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AlertService} from '../alert.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-activity',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-activity.component.css']
 })
 export class AddActivityComponent implements OnInit {
+  addActivityForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private alertService: AlertService,
+              private router: Router) { }
+
+  get f() { return this.addActivityForm.controls; } // accessibel from HTML
 
   ngOnInit() {
+    this.addActivityForm = this.formBuilder.group({
+      title: ['', Validators.compose([Validators.required, Validators.maxLength(50)])],
+      category: ['', [Validators.required]],
+      timeHour: ['0', [Validators.required]],
+      timeMin: ['0', [Validators.required]],
+      timeSec: ['0', [Validators.required]],
+      distance: ['0', [Validators.required]],
+      description: ['', [Validators.maxLength(1000)]],
+    });
   }
 
+  onSubmit() {
+    this.submitted = true;
+  }
 }
